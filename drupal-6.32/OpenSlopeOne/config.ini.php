1193b31a52fe78040b39d07efff67dfb4da431c8
Yueying Tang <yueying.catherine@gmail.com>
2014-08-27 18:11:48 -0400

    Add OpenSlopeOne
    Open sourced collaborative filtering tool OpenSlopeOne

---
+++ drupal-6.32/OpenSlopeOne/config.ini.php
@@ -0,0 +1,8 @@
+; <?php exit; ?> DO NOT REMOVE THIS LINE
+[database]
+host                 = localhost
+username             = supportgroup
+password             = Change%Me1234
+dbname               = supportgrouptest
+port                 = 3306
+adapter              = PDO_MYSQL ; PDO_MYSQL or MYSQLI
